f58266bfc809ddc618e52ac1b4999d61adf1a284
TheJat <manishkl543@gmail.com>
2025-12-21 23:32:37 +0530

    implement storing vault in chrome storage

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,8 +1,35 @@
-const btn = document.getElementById("ping")!;
-const out = document.getElementById("out")!;
+import { deriveKey } from "../crypto/kdf";
+import { randomBytes } from "../crypto/random";
+import { encrypt } from "../crypto/encrypt";
 
-btn.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ type: "PING" }, (res) => {
-    out.textContent = JSON.stringify(res);
+const btn = document.getElementById("setup")!;
+const out = document.getElementById("out")!;
+const input = document.getElementById("password") as HTMLInputElement;
+
+console.log("POPUP SCRIPT LOADED");
+
+btn.addEventListener("click", async () => {
+    console.log("SETUP CLICKED");
+  const password = input.value;
+  if (!password) return;
+
+  const salt = randomBytes(16);
+  const key = await deriveKey(password, salt, ["encrypt", "decrypt"]);
+
+  const emptyVault = new TextEncoder().encode(
+    JSON.stringify({ entries: [] })
+  );
+
+  const iv = randomBytes(12);
+  const encrypted = await encrypt(key, emptyVault, iv);
+
+  chrome.storage.local.set({
+    vault: {
+      salt: Array.from(salt),
+      iv: Array.from(iv),
+      data: Array.from(new Uint8Array(encrypted))
+    }
   });
+
+  out.textContent = "Vault created securely 🔐";
 });
